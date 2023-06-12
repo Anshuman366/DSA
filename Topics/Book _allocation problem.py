d3c3031dcd560ad3274c30f2c51dadd68b8be555
Anshuman366 <anshutiwarimmmm@gmail.com>
2023-06-12 09:57:47 +0530

    Book Allocation Added

--- Topics/Book _allocation problem.py
+++ Topics/Book _allocation problem.py
@@ -1,6 +1,48 @@
-def is_possible(arr,m,n):
-    start=0
-    end=sum(arr)
-    mid=int((start+end)/2)
-    while(start<=end):
+def IsPossible(lst:list , m:int,mid :int):
+   
+    student_count=1
+    
+    pagesum=0
         
+    for i in range(len(lst)):
+            
+            if(lst[i]+pagesum <= mid):
+                pagesum+=lst[i]
+                
+            else: 
+                
+                student_count=student_count+1
+                if(student_count>m or lst[i]>mid):
+                    return False
+                pagesum=lst[i]
+    return True          
+                
+def findPages(arr: [int], n=0, m=0) -> int:
+    start=0
+    
+    end=sum(arr)
+    
+    mid =int((start+end)/2)
+    
+    ans=0
+
+    if len(arr)<m:
+        return -1
+        
+    else:
+
+        while(start<=end):
+            
+            if(IsPossible(arr,m,mid)):
+                        
+                ans=mid
+                end=mid-1
+                #print(start,end)
+            else:
+                start=mid+1
+            mid =int((start+end)/2)
+        return ans
+                    
+#in this the mid will give the sum of the pages which is min because 
+# if we found the possible so;ution thern we will  do  end=mid-1
+#  
